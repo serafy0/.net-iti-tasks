32418092875ac9c19c8a0094febb143bc18e14b5
Mohamed El-Serafy <elserafy02@gmail.com>
2024-07-03 03:12:53 +0300

    day5

--- OOP/day2/task1/Program.cs
+++ OOP/day2/task1/Program.cs
@@ -6,11 +6,11 @@ class Program
     {
         int[] array1 = new int[10];
 
-        for(int i =0;i<array1.Length;i++){
+        for (int i = 0; i < array1.Length; i++)
+        {
             Console.WriteLine($"Enter number at index {i}");
 
             array1[i] = int.Parse(Console.ReadLine());
-
         }
 
         Console.WriteLine($"max is {getMax(array1)}");
@@ -20,78 +20,71 @@ class Program
 
         int search = int.Parse(Console.ReadLine());
 
-        searchNumber(array1,search);
+        searchNumber(array1, search);
 
         bruteForceSort(array1);
 
-        for(int i =0;i<array1.Length;i++){
+        for (int i = 0; i < array1.Length; i++)
+        {
             Console.WriteLine($"new sorted #{i} is {array1[i]} ");
-
         }
-        
-
-
-
-
-        // for (int i = 0; i < array1.Length; i++)
-        //     {
-        //        Console.WriteLine($" number added at index {i} is {array1[i]}");
-        //     } 
-
-
-
     }
 
-    static int  getMax(int[] arr){
+    static int getMax(int[] arr)
+    {
         int max = arr[0];
 
-        for(int i =1;i<arr.Length;i++){
-            if(arr[i]>max)
-            max = arr[i];
-            
+        for (int i = 1; i < arr.Length; i++)
+        {
+            if (arr[i] > max)
+                max = arr[i];
         }
-    return max;
-
+        return max;
     }
 
-    static int  getMin(int[] arr){
+    static int getMin(int[] arr)
+    {
         int min = arr[0];
 
-        for(int i =1;i<arr.Length;i++){
-            if(arr[i]<min)
-            min = arr[i];
-            
+        for (int i = 1; i < arr.Length; i++)
+        {
+            if (arr[i] < min)
+                min = arr[i];
         }
-    return min;
-
+        return min;
     }
 
-    static void searchNumber(int[] arr, int searchValue){
+    static void searchNumber(int[] arr, int searchValue)
+    {
         bool notFound = true;
-        for(int i =0;i<arr.Length;i++){
-            if(arr[i]==searchValue){
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] == searchValue)
+            {
                 notFound = false;
                 Console.WriteLine($"{searchValue} is at index#{i}");
-                
-            }        
+            }
         }
-        if(notFound){
-        Console.WriteLine($"{searchValue} not found");
+        if (notFound)
+        {
+            Console.WriteLine($"{searchValue} not found");
         }
-
     }
 
-    static int[] bruteForceSort(int [] arr){
-        for(int i =0;i<arr.Length;i++){
-           for(int j = 0; j < arr.Length - i - 1; j++){
-            if(arr[j]>arr[j+1]){
-                int temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
+    static int[] bruteForceSort(int[] arr)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            for (int j = 0; j < arr.Length - i - 1; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
             }
-           }    
         }
-    return arr;
-
+        return arr;
     }
 }
--- OOP/day3/task2/task2/Program.cs
+++ OOP/day3/task2/task2/Program.cs
@@ -57,54 +57,63 @@ class Program
         for (int i = 0; i < userArray.Length; i++)
         {
             userArray[i] = new Employee();
-            // userArray[i].setSalary(0);
-            // userArray[i].setSNN($"{i}");
         }
 
-        string input = "N";
-        int[] indexArray = [13];
-        int indexCounter = 0;
         do
         {
-            int newIndex = 15;
+            string input = "N";
+            int[] indexArray = new int[13];
+            for (int i = 0; i < indexArray.Length; i++)
+            {
+                indexArray[i] = -1;
+            }
+            int indexCounter = 0;
             do
             {
-                Console.WriteLine("enter a number between 0 and 12");
+                int newIndex = 15;
+                do
+                {
+                    Console.WriteLine("enter a number between 0 and 12");
 
-                newIndex = int.Parse(Console.ReadLine());
-            } while (newIndex < 0 && newIndex > 12);
+                    newIndex = int.Parse(Console.ReadLine());
+                } while (newIndex < 0 || newIndex > 12);
 
-            // userArray[i]
-            Console.WriteLine("add snn");
-            string newSnn = Console.ReadLine();
-            if (employeeExsistsInArray(userArray, newSnn))
+                Console.WriteLine("add snn");
+                string newSnn = Console.ReadLine();
+                if (employeeExsistsInArray(userArray, newSnn))
+                {
+                    Console.WriteLine("an employee already exists with that snn. try again ");
+                    continue;
+                }
+                else
+                {
+                    userArray[newIndex].setSNN(newSnn);
+                    Console.WriteLine($"add {userArray[newIndex].getSNN()}'s salary:");
+
+                    int newSal = int.Parse(Console.ReadLine());
+
+                    userArray[newIndex].setSalary(newSal);
+                    indexArray[indexCounter] = newIndex;
+                    indexCounter++;
+                }
+
+                Console.WriteLine("enter N if you want to stop the input");
+                input = Console.ReadLine();
+            } while (input != "N");
+
+            Console.WriteLine("this is what you added");
+
+            for (int i = 0; i < indexArray.Length; i++)
             {
-                Console.WriteLine("the array has an employee withy that snn. try again ");
-                continue;
+                // Employee updatedEmpolyee = userArray[indexArray[i]];
+                if (indexArray[i] != -1)
+                {
+                    Console.WriteLine(
+                        $"number{indexArray[i]}: {userArray[indexArray[i]].toString()}"
+                    );
+                }
             }
-            else
-            {
-                userArray[newIndex].setSNN(newSnn);
-                Console.WriteLine($"add {userArray[newIndex].getSNN()}'s salary:");
-
-                int newSal = int.Parse(Console.ReadLine());
-
-                userArray[newIndex].setSalary(newSal);
-                // indexArray.Append(newIndex);
-                indexArray[indexCounter] = newIndex;
-                indexCounter = indexCounter + 1;
-            }
-
-            Console.WriteLine("enter N if you want to stop the input");
-            input = Console.ReadLine();
-        } while (input != "N");
-
-        Console.WriteLine("this is what you added");
-
-        for (int i = 0; i < indexArray.Length; i++)
-        {
-            Console.WriteLine($"number{indexArray[i]}: {userArray[indexArray[i]].toString()}");
-        }
+        } while (true);
     }
 
     static Employee getEmployeeBySNN(Employee[] empArray, string snnValue)
