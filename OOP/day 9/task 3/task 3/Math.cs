1b5fea6c4b9ea9910620606a6762f3b9b87330d7
Mohamed El-Serafy <elserafy02@gmail.com>
2024-07-09 07:10:39 +0300

    task 3

---
+++ OOP/day 9/task 3/task 3/Math.cs
@@ -0,0 +1,24 @@
+namespace task_3;
+
+static class Math
+{
+    public static double Add(double left, double right)
+    {
+        return (left + right);
+    }
+
+    public static double Subtract(double left, double right)
+    {
+        return (left - right);
+    }
+
+    public static double Multiply(double left, double right)
+    {
+        return (left * right);
+    }
+
+    public static double Divide(double left, double right)
+    {
+        return (left / right);
+    }
+}
---
+++ OOP/day 9/task 3/task 3/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace task_3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Math.Add(1, 2));
+        Console.WriteLine(Math.Subtract(1, 2));
+        Console.WriteLine(Math.Multiply(1, 2));
+        Console.WriteLine(Math.Divide(1, 2));
+    }
+}
