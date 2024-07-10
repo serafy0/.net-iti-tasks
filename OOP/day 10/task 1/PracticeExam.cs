062edb62d0577eabcf2db02260044eaba727a410
Mohamed El-Serafy <elserafy02@gmail.com>
2024-07-10 08:34:09 +0300

    day 10

--- OOP/day 10/task 1/PracticeExam.cs
+++ OOP/day 10/task 1/PracticeExam.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 namespace task_1;
 
 class PracticeExam : FinalExam
@@ -8,5 +10,25 @@ class PracticeExam : FinalExam
     public override void ShowExam()
     {
         base.ShowExam();
+
+        foreach ((var _q, var _a) in QuestionsMap)
+        {
+            Console.WriteLine(_q.ToString());
+            Console.WriteLine("the correct answer(s) was");
+
+            foreach (Answer i in _a)
+            {
+                StringBuilder sb = new StringBuilder();
+
+                sb.Append(i.Letter);
+                sb.Append(") ");
+                sb.Append(i.Body);
+                sb.Append("\n");
+
+                Console.WriteLine(sb);
+            }
+        }
+
+        Console.WriteLine($"your practice grade={FinalGrade}");
     }
 }
