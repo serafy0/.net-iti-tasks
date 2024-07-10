23a9ce46ebca4e8de8870d8ff461b29951fc002e
Mohamed El-Serafy <elserafy02@gmail.com>
2024-07-10 14:27:39 +0300

    day 11

--- OOP/day 11/task 1/Program.cs
+++ OOP/day 11/task 1/Program.cs
@@ -6,23 +6,23 @@ class Program
     {
         List<Book> books = Repository.Books;
         // User Defined Delegate Datatype
-        LibraryEngine.BookDelegate fPtr1 = BookFunctions.GetTitle;
-        LibraryEngine.ProcessBooks(books, fPtr1);
+        LibraryEngine.BookDelegate del1 = BookFunctions.GetTitle;
+        LibraryEngine.ProcessBooks(books, del1);
 
         // BCL Delegates
-        Func<Book, string> fPtr2 = new Func<Book, string>(BookFunctions.GetAuthors);
-        LibraryEngine.ProcessBooks(books, fPtr2);
+        Func<Book, string> del2 = new Func<Book, string>(BookFunctions.GetAuthors);
+        LibraryEngine.ProcessBooks(books, del2);
 
         // Anonymous Method (GetISBN)
-        LibraryEngine.BookDelegate fPtr3 = (Book book) =>
+        LibraryEngine.BookDelegate del3 = (Book book) =>
         {
             return book.ISBN;
         };
 
-        LibraryEngine.ProcessBooks(books, fPtr3);
+        LibraryEngine.ProcessBooks(books, del3);
 
         // Lambda Expression (GetPublicationDate)
-        LibraryEngine.BookDelegate fPtr4 = book => book.PublicationDate.ToString();
-        LibraryEngine.ProcessBooks(books, fPtr4);
+        LibraryEngine.BookDelegate del4 = book => book.PublicationDate.ToString();
+        LibraryEngine.ProcessBooks(books, del4);
     }
 }
